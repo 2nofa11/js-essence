23a79a57fec19fd306efd8951defa1453a70275b
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-07 15:18:49 +0900

    feat: スコープチェーン

--- 030_スコープ/040_スコープチェーン/start/main.js
+++ 030_スコープ/040_スコープチェーン/start/main.js
@@ -1,18 +1,14 @@
+// スコープが別スコープにまたがっている状態
+// 内側スコープまで探しに行って、次スコープをみる
+// グローバルスコープは一番外側
 let a = 2;
 window.a = 4;
 function fn1() {
-    // let a = 1;
-    function fn2() {
-        console.log(a);
-
-        if (true) {
-            var a = 3;
-        }
-
-    }
-    fn2();
+  //   let a = 1;
+  function fn2() {
+    console.log(a);
+    var a = 3;
+  }
+  fn2();
 }
 fn1();
-
-
-
