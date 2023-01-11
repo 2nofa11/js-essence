8ba9dc3f65b9d793f3ab09c7d9d81675abaebf9c
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-12 08:38:10 +0900

    Q:thisって？ コンテキスト内での呼び出し元のアドレスを指す

--- 050_関数とオブジェクト１/040_this/start/main.js
+++ 050_関数とオブジェクト１/040_this/start/main.js
@@ -1,6 +1,10 @@
+// 呼び出し元のオブジェクトへの参照を保持するキーワード
+// →実行コンテキストによって変わる
 const person = {
-    name: 'Tom',
-    hello: function() {
+  name: "Tom",
+  hello: function () {
+    console.log("hello" + this.name);
+  },
+};
 
-    }
-}
+person.hello();
