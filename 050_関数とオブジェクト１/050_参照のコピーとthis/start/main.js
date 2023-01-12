6d0037ea0fad8abafb5ebe553c824776f78d66a6
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-13 07:41:32 +0900

    feat: オブジェクトのメソッドか、関数化でthisのコンテキストは変わる

--- 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
+++ 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
@@ -1,7 +1,25 @@
+window.name = "Jhon";
+
 const person = {
-    name: 'Tom',
-    hello: function() {
-        console.log('Hello ' + this.name);
-    }
+  name: "Tom",
+  hello: function () {
+    console.log("Hello " + this.name);
+    a();
+    const person = {
+      name: "hoge",
+      hello: function () {
+        console.log("Hello " + this.name);
+        a();
+      },
+    };
+    person.hello();
+  },
+};
+// personとは切り離される
+const ref = person.hello;
+ref();
+// ★オブジェクトのメソッドか、関数かで変わる
+
+function a() {
+  console.log("hello" + this.name);
 }
-person.hello();
