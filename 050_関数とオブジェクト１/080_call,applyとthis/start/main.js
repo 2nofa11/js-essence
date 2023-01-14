bc689549f9be06bf0729c28f8cd3cb50c930b565
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-14 22:35:15 +0900

    callとapplyを使かってthisを指定する

--- 050_関数とオブジェクト１/080_call,applyとthis/start/main.js
+++ 050_関数とオブジェクト１/080_call,applyとthis/start/main.js
@@ -1,7 +1,15 @@
-function a(name) {
-    console.log('hello ' + name);
+function a(lname) {
+  console.log("hello " + this.name + lname);
 }
 
-const b = a.bind(null, 'Tim');
+const b = a.bind({ name: "Tim" }, "cook");
 
 b();
+const tim = { name: "Tim" };
+a.apply(tim, ["cook"]); //関数が実行まで行われる
+a.call(tim, "cook");
+
+const array = [1, 9, 2];
+
+console.log(Math.max.apply(null, array));
+console.log(Math.max(...array));
