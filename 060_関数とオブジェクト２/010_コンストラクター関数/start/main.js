bdb45aa71257ebc5c8704de6225d23c1aea78381
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-16 07:34:49 +0900

    コンストラクタ関数はthisに値を入れ込む、また、コンストラクター関数は複製するときに便利

---
+++ 060_関数とオブジェクト２/010_コンストラクター関数/start/main.js
@@ -0,0 +1,12 @@
+// ひな形となる関数
+
+// const obj = new A()
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const bob = new Person("bob", 18);
+const tom = new Person("tom", 20);
+const sun = new Person("sun", 30);
