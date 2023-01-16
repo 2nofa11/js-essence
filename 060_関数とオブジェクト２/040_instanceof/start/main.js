7319676fafcb28c3e5844d8fee970c43b038aca0
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-17 07:31:17 +0900

    instanceofを使って、オブジェクトのコンストタを割りだせる

--- 060_関数とオブジェクト２/040_instanceof/start/main.js
+++ 060_関数とオブジェクト２/040_instanceof/start/main.js
@@ -1,7 +1,28 @@
 function F(a, b) {
-    this.a = a;
-    this.b = b;
-    // return {a: 1};
+  this.a = a;
+  this.b = b;
+  //   const result = new Object();
+  //   result.a = 1;
+  //   return result;
 }
 
-F.prototype.c = function() {}
+F.prototype.c = function () {};
+
+const instance = new F(1, 2);
+console.log(instance);
+console.log(instance instanceof F);
+console.log(instance.__proto__ === Object.prototype); // instanseofはprototypeの値を確認している
+
+// instanceof ⇒作成元のコンストラクターが割り出せる
+// ⇒条件分岐が使えるようになる
+
+function fn(arg) {
+  if (arg instanceof Array) {
+    arg.push("value");
+  } else {
+    arg["key"] = "value";
+  }
+  console.log(arg);
+}
+fn({});
+fn([]);
