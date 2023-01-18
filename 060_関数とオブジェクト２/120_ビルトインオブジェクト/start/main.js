1f2f8ddd7d55808cc26de370be57f3d42be4e48d
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-18 18:01:45 +0900

    ビルドインオブジェクト：JSですでに用意しているオブジェクト群

---
+++ 060_関数とオブジェクト２/120_ビルトインオブジェクト/start/main.js
@@ -0,0 +1,7 @@
+// 実行前に生成されるオブジェクト
+const array = new Array(1, 2, 3);
+const array2 = [1, 2, 3];
+
+console.log(array[0]);
+console.log(array.hasOwnProperty(0));
+console.log(array.hasOwnProperty(4));
