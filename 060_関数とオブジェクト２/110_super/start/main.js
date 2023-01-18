dbbe2bdfafe70ca1296ebf1793a9b6ce6ed29110
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-18 17:53:44 +0900

    superは基本はクラスの中で使われるもの

--- 060_関数とオブジェクト２/110_super/start/main.js
+++ 060_関数とオブジェクト２/110_super/start/main.js
@@ -1,43 +1,49 @@
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
 
-    hello() {
-        console.log('hello ' + this.name);
-    }
-}
-
-class Japanese extends Person {
-    constructor(name, age, gender) {
-        super(name, age);
-        this.gender = gender;
-    }
-
-    hello() {
-        super.hello();
-        console.log('Konnichiwa ' + this.name);
-    }
-
-    bye() {
-        console.log('Sayonara ' + this.name);
-    }
-}
-
-const taro = new Japanese('Taro', 23, 'Male');
-console.log(taro);
-taro.bye();
-
-// const american = {
-//     hello() {
-//         console.log('hello ' + this.name);
-//     }
+//   hello() {
+//     console.log("hello " + this.name);
+//   }
 // }
 
-// const bob = {
-//     name: 'Bob',
-//     hello() {
-//         super.hello();
-//     }
+// class Japanese extends Person {
+//   constructor(name, age, gender) {
+//     super(name, age); //ほかのクラスを継承したコンストラクタ
+//     this.gender = gender;
+//   }
+
+//   hello() {
+//     super.hello();
+//     console.log("Konnichiwa " + this.name);
+//   }
+
+//   bye() {
+//     console.log("Sayonara " + this.name);
+//   }
 // }
+
+// // 関数コンテキストの一つ。特別な条件下で使用可能
+// // 基本はクラス内で使用したい
+
+// const taro = new Japanese("Taro", 23, "Male");
+// console.log(taro);
+// taro.bye();
+// taro.hello();
+const american = {
+  hello() {
+    console.log("hello " + this.name);
+  },
+};
+
+const bob = {
+  name: "Bob",
+  hello() {
+    super.hello();
+    console.log("hello " + this.name);
+  },
+};
+Object.setPrototypeOf(bob, american);
+bob.hello();
