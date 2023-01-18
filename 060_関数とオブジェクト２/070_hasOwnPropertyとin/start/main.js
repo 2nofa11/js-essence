bd83e4328225996f836e28a7a3e9b8916e314ced
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-18 16:17:05 +0900

    feat: プロトタイプチェーンにいるかを調べるのには`in`が必要

--- 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
+++ 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
@@ -1,10 +1,13 @@
 function Person(name, age) {
-    this.name = name;
-    this.age = age;
+  this.name = name;
+  this.age = age;
 }
 
-Object.prototype.hello = function() {
-    console.log('Object: hello ' + this.name);
-}
+Object.prototype.hello = function () {
+  console.log("Object: hello " + this.name);
+};
 
-const bob = new Person('Bob', 18);
+const bob = new Person("Bob", 18);
+const result = bob.hasOwnProperty("hello"); // オブジェクトのプロパティにいるか
+console.log(result);
+console.log("hasOwnProperty" in bob); //指定された名前のメソッドをPTC含めて調べる
