e2c9bec82d4042d4be27787fcca0512df50b5c8c
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-18 16:48:21 +0900

    feat: classがコンストラクタとプロトタイプを使ってできているものを知った衝撃

--- 060_関数とオブジェクト２/090_クラス/start/main.js
+++ 060_関数とオブジェクト２/090_クラス/start/main.js
@@ -1,8 +1,23 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  hello() {
+    console.log("hello " + this.name);
+  }
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
-}
+const bob = new Person("bob", 20);
+console.log(bob);
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+// Person.prototype.hello = function () {
+//   console.log("hello " + this.name);
+// };
+
+// クラスはコンストラクタ関数をクラス表記で書けるようにしたもの
+// 簡単にかけうようにした、シンタックスシュガー
+// 裏側で動いている仕組みを理解できてるとガチで学べてる！
