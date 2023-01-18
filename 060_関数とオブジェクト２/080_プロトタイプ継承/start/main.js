ba7628ad10a120771946e47ee468dc0536cc93f2
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-18 16:35:17 +0900

    feat: コンストラクタの中身はcallで、プロトタイプの継承はObject.createで行う

--- 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -3,6 +3,25 @@ function Person(name, age) {
   this.age = age;
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
+Person.prototype.hello = function () {
+  console.log("hello " + this.name);
+};
+
+// 別コンストラクター関数のプロトタイプを受け継いで、
+// 機能を流用できるようにすること。(コンストラクタ間で機能の受け渡しができる)
+
+// extendsはクラスの継承
+
+function Japanese(name, age, gender) {
+  // 関数コンテキストはThis
+  Person.call(this, name, age);
+  this.gender = gender;
 }
+
+Japanese.prototype = Object.create(Person.prototype);
+const taro = new Japanese("taro", 21, "men");
+console.log(taro);
+
+Japanese.prototype.bye = function () {
+  console.log("さよなら" + this.name);
+};
