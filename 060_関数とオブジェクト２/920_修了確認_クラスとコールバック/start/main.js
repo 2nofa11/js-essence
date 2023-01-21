4f44aacbf3b689aaa442594547d8ba02bdda6998
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-21 21:42:09 +0900

    settimeotは型意識するとわかりやすい

--- 060_関数とオブジェクト２/920_修了確認_クラスとコールバック/start/main.js
+++ 060_関数とオブジェクト２/920_修了確認_クラスとコールバック/start/main.js
@@ -2,22 +2,32 @@
  * 問題：
  * Person.helloメソッドをsetTimeoutで１秒後に
  * 実行しようとしましたが、"hello Bob"と表示されませんでした。
- * 
+ *
  * setTimeoutに渡す方法がおかしそうなのですが、
  * どのようにすればよいでしょうか？
- * 
+ *
  * ※２通りの方法で実装してみてください。
  */
 class Person {
   constructor(name, age) {
-      this.name = name;
-      this.age = age;
+    this.name = name;
+    this.age = age;
   }
 
   hello() {
-      console.log('hello ' + this.name);
+    console.log("hello " + this.name);
   }
 }
 
-const bob = new Person('Bob', 23);
-setTimeout(bob.hello, 1000);
+// オブジェクトとのメソッドをCBで渡した場合
+// 参照しているハローがよばれる
+const bob = new Person("Bob", 23);
+setTimeout(bob.hello.bind(bob), 1000);
+
+setTimeout(() => {
+  bob.hello();
+}, 1000);
+
+// setTimeout(function () {
+//   bob.hello();
+// }, 2000);
