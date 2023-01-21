164173f10831672c48c5885488dc5c5da1091284
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-21 22:05:27 +0900

    自分の回答

--- 060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
+++ 060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
@@ -2,21 +2,21 @@
  * 問題：
  * 電卓の入力と同じような挙動をするチェーンメソッド
  * を作成してみましょう。
- * 
+ *
  * 例えば、次のように使用し、結果が表示
  * されるようにします。
- * 
- * 
+ *
+ *
  * 例１）
  * const calc = new Calculator();
- * 
+ *
  * calc.set(10)
  * .minus()
  * .set(3) -> '7'を出力(10 - 3)
- * 
+ *
  * 例２）
  * const calc = new Calculator();
- * 
+ *
  * calc.set(10)
  * 	.minus()
  * 	.set(3) -> '7'を出力
@@ -24,14 +24,54 @@
  * 	.set(6) -> '42'を出力（10 - 3) * 6
  */
 
-const calc = new Calculator();
+// _は外から操作するな
+class Calculator {
+  constructor() {
+    this.count = 0;
+    this._operator = "";
+  }
+  set(num) {
+    switch (this._operator) {
+      case "plus":
+        this.count = this.count + num;
+        console.log(this.count);
+        break;
+      case "minus":
+        this.count = this.count - num;
+        console.log(this.count);
+        break;
+      case "mutiply":
+        this.count = this.count * num;
+        console.log(this.count);
+        break;
+      case "divide":
+        this.count = this.count / num;
+        console.log(this.count);
+        break;
+      default:
+        this.count = num;
+        break;
+    }
+    this._operator = "";
+    return this;
+  }
+  plus() {
+    this._operator = "plus";
+    return this;
+  }
+  minus() {
+    this._operator = "minus";
+    return this;
+  }
+  divide() {
+    this._operator = "divide";
+    return this;
+  }
+  mutiply() {
+    this._operator = "mutiply";
+    return this;
+  }
+}
 
-calc.set(10)
-	.minus()
-	.set(3)
-	.mutiply()
-	.set(6)
-	.divide()
-	.set(2)
-	.plus()
-	.set(2)
+const calc = new Calculator();
+calc.set(10).minus().set(3).mutiply().set(6).divide().set(2).plus().set(2);
