d214b2f383dc6ca5e678c1e7e6414c079a6d6f00
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-23 21:55:43 +0900

    forにて対応する

--- 070_反復処理/030_配列とループ文/start/main.js
+++ 070_反復処理/030_配列とループ文/start/main.js
@@ -1,1 +1,17 @@
 const arry = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arry.length; i++) {
+//   console.log(i);
+// }
+
+// let j = 0;
+// while (j < arry.length) {
+//   console.log(j);
+//   j++;
+// }
+
+let v = 0;
+let i = 0;
+while ((v = arry[i++])) {
+  console.log(v);
+}
