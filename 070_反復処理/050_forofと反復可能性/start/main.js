a679e7ec330d27d06e492aff73e48c8abdb18898
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-24 20:20:55 +0900

    forofはイテレーターの挙動に依存する。（Array等が持っている組み込みオブジェクト）

--- 070_反復処理/050_forofと反復可能性/start/main.js
+++ 070_反復処理/050_forofと反復可能性/start/main.js
@@ -1,1 +1,16 @@
-const arry = ['a', 'b', 'c'];
+const arry = ["a", "b", "c"];
+
+// 反復操作を行う際に使用するイテレーターにて反復
+arry[4] = "e";
+
+Object.prototype.method = function () {};
+
+Object.defineProperty(arry, 0, {
+  enumerable: false,
+});
+const d = Object.getOwnPropertyDescriptor(arry, 0);
+console.log(d);
+
+for (let v of arry) {
+  console.log(v);
+}
