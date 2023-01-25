2381c08eeec1ab61f07f4a679249e48b3fe8d43a
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-26 07:40:33 +0900

    thisの使いかたムズイ。 分割代入で配列って使えるんだ

--- 070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
+++ 070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
@@ -1,10 +1,26 @@
-const obj = {
-	prop1: 'value1',
-	prop2: 'value2',
-	prop3: 'value3'
-}
+const items = {
+  prop1: "value1",
+  prop2: "value2",
+  prop3: "value3",
+};
 
-const items = Object.entries(obj);
-for (let item of items) {
-	console.log(item);
+// const items = Object.entries(obj); //for of配列のため
+
+Object.prototype[Symbol.iterator] = function () {
+  //   console.log(this);
+  let _this = this;
+  const keys = Object.keys(this); //プロパティを配列
+  //   console.log(keys); レキシカルスコープとスコープチェーン参照
+  let i = 0;
+  return {
+    next() {
+      //   console.log(this);
+      let key = keys[i++];
+      return { value: [key, _this[key]], done: i > keys.length };
+    },
+  };
+};
+
+for (let [j, k] of items) {
+  console.log(j);
 }
