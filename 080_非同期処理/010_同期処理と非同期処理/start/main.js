c608b0598a052ffbb45707214096387a2a73104c
2nofa11 <aboubthethirdkimi@icloud.com>
2023-01-29 14:49:56 +0900

    非同期処理は一時的に処理が切り離される

--- 080_非同期処理/010_同期処理と非同期処理/start/main.js
+++ 080_非同期処理/010_同期処理と非同期処理/start/main.js
@@ -1,10 +1,20 @@
+// スレッド：一本の糸（mainスレッド）
+// JS実行>レンダリング
+// 同期処理ではメインスレッドでコードが順番に実行される（スレッド遵守）
+
 function sleep(ms) {
   const startTime = new Date();
   while (new Date() - startTime < ms);
-  console.log('sleep done');
+  console.log("sleep done");
 }
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', function(){
-  console.log('button clicked');
+const btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  console.log("button clicked");
 });
+
+setTimeout(() => {
+  sleep(3000);
+}, 3000); //メインスレッドが開放される
+
+// 一時的にメインスレッドから処理をきりはなす
