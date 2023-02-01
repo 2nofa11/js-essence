38770896344168d094dde030fc565e0d57e054b2
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-01 21:41:03 +0900

    Promiseをチェーンさせないと非同期処理は連続しない

--- 080_非同期処理/060_Promiseチェーン/start/main.js
+++ 080_非同期処理/060_Promiseチェーン/start/main.js
@@ -1,6 +1,42 @@
-function sleep(callback, val) {
-  setTimeout(function() {
-    console.log(val++);
-    callback(val);
-  }, 1000);
+function sleep(val) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log(val++);
+      resolve(val);
+    }, 1000);
+  });
 }
+
+sleep(0)
+  .then((val) => sleep(val))
+  .then(function (val) {
+    return sleep(val);
+  })
+  .then(function (val) {
+    // sleep(val);
+    // return val;
+    return sleep(val);
+  })
+  .then(function (val) {
+    return sleep(val);
+  })
+  .then(function (val) {
+    return sleep(val);
+  })
+  .then(function (val) {
+    return sleep(val);
+  });
+
+// リターンにPromiseのインスタンスを書いてあげるチェーンが切れてしまう
+
+// new Promise(function (resolve, reject) {
+//   resolve(
+//     sleep(function (val) {
+//       return val;
+//     }, 1)
+//   );
+// }).then((val) => {
+//   sleep(function (val) {
+//     return val;
+//   }, val);
+// });
