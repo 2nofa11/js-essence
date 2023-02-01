ae954cf3c986682386be6804f02e14e46add594d
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-01 21:55:33 +0900

    並列処理もPromiseを返す

--- 080_非同期処理/070_Promiseと並列処理/start/main.js
+++ 080_非同期処理/070_Promiseと並列処理/start/main.js
@@ -1,16 +1,26 @@
 function sleep(val) {
-  return new Promise(function(resolve) {
-    setTimeout(function() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       console.log(val++);
-      resolve(val);
-    }, 1000);
+      reject(val);
+    }, val * 500);
   });
 }
 
-sleep(0).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-})
+// 反復可能オブジェクト
+Promise.allSettled([sleep(2), sleep(5)]).then(function (d) {
+  console.log(d);
+});
+// sleep(0)
+//   .then(function (val) {
+//     return Promise.all([sleep(2), sleep(5)]);
+//   })
+//   .then(function (val) {
+//     console.log(val);
+//     return sleep(val);
+//   })
+//   .then(function (val) {
+//     return sleep(val);
+//   });
+
+// 反復可能オブジェクトにPriomiseのインスタンスを追加する
