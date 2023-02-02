afd875e2108dad9ce62be47e889e869d7a069ae4
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-02 20:45:51 +0900

    マクロとマイクロが絡み合ったときにどのような処理がなされるのかを説明した

--- 080_非同期処理/090_MacrotasksとMicrotasks（Part. 2）/start/main.js
+++ 080_非同期処理/090_MacrotasksとMicrotasks（Part. 2）/start/main.js
@@ -1,13 +1,32 @@
+//　マイクロタスクとマクロタスクが混ざっている
 new Promise(function promise(resolve) {
-  console.log('promise');
+  console.log("promise");
 
   setTimeout(function task1() {
-    console.log('task1');
+    console.log("task1");
+    // 非同期処理内にresolveが存在する。
     resolve();
   });
-
-}).then(function job1() {
-  console.log('job1');
 })
+  .then(function job1() {
+    console.log("job1");
+    setTimeout(function task1() {
+      console.log("task2");
+      queueMicrotask(function job4() {
+        console.log("job4");
+      });
+    });
 
-console.log('global end');
+    const p = Promise.resolve();
+    p.then(() => {
+      console.log("job5");
+    });
+  })
+  .then(function job2() {
+    console.log("job2");
+  })
+  .then(function job3() {
+    console.log("job3");
+  });
+
+console.log("global end");
