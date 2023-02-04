250dfd92867cf71e7becda9dc5a1b813f06ab711
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-04 10:18:40 +0900

    スローでエラーが投げられた場合には、ハンドリングを適切に行う

--- 080_非同期処理/120_例外処理とエラー/start/main.js
+++ 080_非同期処理/120_例外処理とエラー/start/main.js
@@ -1,11 +1,46 @@
+// エラーが発生した際に飛ぶ特別な処理
 
-
-// async function fetchUsers() {
-//   const response = await fetch('users.json');
-//   const json = await response.json();
-//   for(const user of json) {
-//     console.log(`I'm ${user.name}, ${user.age} years old`)
-//   }
+// try {
+//   console.log("s");
+//   throw new Error("eです");
+//   console.log("e");
+// } catch (e) {
+//   console.log(e);
+// } finally {
+//   console.log("bye");
 // }
+async function fetchUsers() {
+  const response = await fetch("users.json");
+  if (response.ok) {
+    const json = await response.json();
+    if (!json.length) {
+      throw new NoDataError("np data found");
+    }
+    return json;
+  }
+}
 
-// fetchUsers();
+class NoDataError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "NoDataError";
+  }
+}
+
+async function init() {
+  try {
+    const users = await fetchUsers();
+    for (const user of users) {
+      console.log(`I'm ${user.name}, ${user.age} years old`);
+    }
+  } catch (e) {
+    // 条件分岐とかでカスタムエラーを判定するこができるようになる
+    if (e instanceof NoDataError) {
+      console.log(e);
+    }
+  } finally {
+    console.log("bye");
+  }
+  console.log("end");
+}
+init();
