bdbd03364a66bdead6f03f53f0f7176fd77a7d2f
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-04 11:31:34 +0900

    自分の回答

--- 080_非同期処理/910_修了確認_Promiseチェーン/start/main.js
+++ 080_非同期処理/910_修了確認_Promiseチェーン/start/main.js
@@ -3,35 +3,50 @@
  * myFetch関数を使って以下の「データ取得の流れ」の通り、
  * 順次JSONを取得し、取得したデータを加工して、
  * 以下のメッセージをコンソールに表示してください。
- * 
+ *
  * --Bob's timeline--
  * Tim says: waiting at station.
  * Sun says: go shopping?
  * John says: how's it going?
  * Sun says: drink?
  *
- ******************************   
+ ******************************
  * データ取得の流れ
  * １．"user1.json"を取得（Bobのユーザー情報取得）
- * 
+ *
  * ２．"user1.json"のidの項目を元に
  *     `friendsOf${id}.json`でフレンド一覧を取得
- * 
+ *
  * ３．取得したフレンドのID（ユーザーID）を元に、
  * 　　`user${id}.json`で各ユーザーの情報を取得
- * 
+ *
  * ４．各ユーザー情報のlatestMsgIdが最後に投稿した
  * 　　メッセージのIDになりますので、そのidを用いて
  * 　　`message${friend.latestMsgId}.json`
  * 　　を取得。
- * 
+ *
  * １～４で取得したデータをもとにコンソールログに
  * タイムラインの文字列を作成してください。
- * 
+ *
  * ※await/asyncで記述してみてください。
  */
 async function myFetch(fileName) {
-	const response = await fetch(`../json/${fileName}`);
-	const json = await response.json();
-	return json;
+  const response = await fetch(`../json/${fileName}`);
+  const json = await response.json();
+  return json;
 }
+
+async function init() {
+  const mainUser = await myFetch("user1.json"); // ユーザ1を取得
+  console.log(`--${mainUser.name}'s timeline--`);
+  //   `friendsOf${id}.json`でフレンド一覧を取得
+  const friendList = myFetch(`friendsOf${mainUser.id}.json`);
+  friendList.then(async (res) => {
+    for (const iterator of res.friendIds) {
+      const friend = await myFetch(`user${iterator}.json`);
+      const friendMsg = await myFetch(`message${friend.latestMsgId}.json`);
+      console.log(friend.name, friendMsg.message);
+    }
+  });
+}
+init();
