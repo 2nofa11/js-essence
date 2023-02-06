c8b6d5081a2237a7a162b45c8518f73e1443990d
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-06 22:15:37 +0900

    クラス内では strictモードが常時ON

--- 090_モジュラーJavaScript/060_Strictモードとクラス/start/main.js
+++ 090_モジュラーJavaScript/060_Strictモードとクラス/start/main.js
@@ -1,10 +1,21 @@
-class C {
-    constructor() {
+function fn() {
+  console.log(this);
+}
+class C1 {
+  constructor() {
+    fn();
+    function fn1() {
+      console.log(this);
     }
+    fn1();
+  }
 
-    method() {
+  method() {
+    function fn() {
+      console.log(this);
     }
+  }
 }
 
-const c = new C();
+const c = new C1();
 c.method();
