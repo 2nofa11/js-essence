bb19f5f1deab1739e8811cebfdab109cd04530c3
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-08 08:45:31 +0900

    オブジェクトの独自操作を実装できる

--- 100_様々なオブジェクト/010_Proxy/start/main.js
+++ 100_様々なオブジェクト/010_Proxy/start/main.js
@@ -1,1 +1,31 @@
+// プロパティの操作に独自の処理を追加するためのオブジェクト
+
 const targetObj = { a: 0 };
+const handler = {
+  set: function (target, prop, value, receiver) {
+    console.log(`[set]: ${prop}`);
+    // target[prop] = value;
+    throw new Error("readonly");
+  },
+  get: function (target, prop, receiver) {
+    console.log(`[get]: ${prop}`);
+
+    if (target.hasOwnProperty(prop)) {
+      return target[prop];
+    } else {
+      return -1; // 値がなかった場合のデフォルト値
+    }
+    // return target[prop];
+  },
+  deleteProperty: function (target, prop) {
+    console.log(`[delete]: ${prop}`);
+    // delete target[prop];
+    throw new Error("readonly");
+  },
+};
+const pxy = new Proxy(targetObj, handler);
+// pxy.a = 1;
+
+// delete pxy.a;
+console.log(pxy.a);
+console.log(pxy.b);
