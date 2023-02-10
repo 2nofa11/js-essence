15012f74462253b57d0196fb3e96a1f6a9adfe84
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-10 23:11:27 +0900

    難しすぎるので保留

--- 100_様々なオブジェクト/030_ReflectとProxy/start/main.js
+++ 100_様々なオブジェクト/030_ReflectとProxy/start/main.js
@@ -1,13 +1,21 @@
-const targetObj = { a: 0 };
-
+const targetObj = {
+  a: 0,
+  get value() {
+    return this.b;
+  },
+};
+// プロキシも同じようなことできる
 const handler = {
-  get: function(target, prop, receiver) {
+  get: function (target, prop, receiver) {
     console.log(`[get]: ${prop}`);
-    if(target.hasOwnProperty(prop)) {
-      return target[prop];
+    console.log(receiver);
+    if (target.hasOwnProperty(prop)) {
+      return Reflect.get(target, prop, receiver); //バインドさせてる！
     } else {
       return -1;
     }
-  }
-}
+  },
+};
 const pxy = new Proxy(targetObj, handler);
+console.log(pxy.value);
+// console.log(pxy.a);
