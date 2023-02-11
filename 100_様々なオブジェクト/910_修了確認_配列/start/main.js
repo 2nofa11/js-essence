0c8b33a9b076e29fe29a1a83306cd04365fdd98f
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-11 12:21:31 +0900

    一次回答

--- 100_様々なオブジェクト/910_修了確認_配列/start/main.js
+++ 100_様々なオブジェクト/910_修了確認_配列/start/main.js
@@ -1,42 +1,69 @@
 /**
  * 問題：
  * Arrayを継承して以下のメソッドを実装してみましょう。
- * 
+ *
  * push(*1)
  * forEach
  * map
  * filter
  * reduce
- * 
+ *
  * *1:pushはチェーンメソッドとしてつなげられるように実装してみてください。
  */
 class MyArray extends Array {
-	constructor(...args) {
-		super(...args)
-	}
+  constructor(...args) {
+    super(...args);
+  }
+  push(num) {
+    super.push(num);
+    return this;
+  }
+  map(func) {
+    let newArry = new MyArray();
+    for (const v of this) {
+      newArry.push(func(v));
+    }
+    return newArry;
+  }
 
-	print(label = '') {
-		console.log(`%c ${label}`, 'color: blue; font-weight: 600;', this);
-		return this;
-	}
+  filter(func) {
+    let newArry = new MyArray();
+    for (const v of this) {
+      if (func(v)) {
+        newArry.push(v);
+      }
+    }
+    return newArry;
+  }
+  print(label = "") {
+    console.log(`%c ${label}`, "color: blue; font-weight: 600;", this);
+    return this;
+  }
+  reduce(func, init = 0) {
+    let returnVal = init;
+    for (const v of this) {
+      returnVal = func(returnVal, v);
+    }
+    return returnVal;
+  }
 }
 
 function double(v, i, obj) {
-	return v * 2;
+  return v * 2;
 }
 
 const original = new MyArray(1, 2, 3, 4);
 
 const result = original
-	.map(double)
-	.push(5)
-	.filter(function (v, i) {
-		return v > 2;
-	})
-	.reduce(function(accu, curr) {
-		return accu + curr;
-	})
+  .map(double)
+  .push(5)
+  .print()
+  .filter(function (v, i) {
+    return v > 2;
+  })
+  .reduce(function (accu, curr) {
+    return accu + curr;
+  });
 
-console.log('%coriginal', 'color: blue; font-weight: bold;', original);
-console.log('%cresult', 'color: red; font-weight: bold;', result);
-
+console.log("%coriginal", "color: blue; font-weight: bold;", original);
+console.log("%cresult", "color: red; font-weight: bold;", result);
