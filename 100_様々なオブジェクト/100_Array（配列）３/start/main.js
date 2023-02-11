5b64cd25e7018fe4f75db4525f4bd5b9edabca94
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-11 09:56:19 +0900

    reduceについて、第二引数の初期値がないとaccuに値が入ってしまうこと知った

--- 100_様々なオブジェクト/100_Array（配列）３/start/main.js
+++ 100_様々なオブジェクト/100_Array（配列）３/start/main.js
@@ -1,1 +1,10 @@
 const arry = [1, 2, 3, 4, 5];
+// 配列を結合して値をまとめる
+
+const result = arry.reduce((accu, curr) => {
+  console.log(accu, curr);
+  return accu + curr * 2;
+}, 0);
+// 最後でconstに返す
+
+console.log(result);
