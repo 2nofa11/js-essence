0a4b4c469f83df7fc8fab286d01e1257a0512151
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-11 09:46:17 +0900

    forEachとmapおよびfilterの説明 配列の場合は反復メソッドが充実している

--- 100_様々なオブジェクト/090_Array（配列）２/start/main.js
+++ 100_様々なオブジェクト/090_Array（配列）２/start/main.js
@@ -1,1 +1,17 @@
 const arry = [1, 2, 3, 4, 5];
+
+// arry.forEach((v) => {
+//   console.log(v);
+// });
+
+const newArry = arry.map((v, i, arry) => {
+  console.log(v);
+  return arry;
+});
+
+// console.log(newArry);
+
+const fillterArry = arry.filter((a) => {
+  return a % 2 === 0;
+});
+console.log(fillterArry);
