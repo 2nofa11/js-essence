24ba018f62c2d6d2722d84f0903fd42b78163e81
2nofa11 <aboubthethirdkimi@icloud.com>
2023-02-13 22:17:49 +0900

    とてもムズイ。 トラックするタイミングがプロパティにアクセスしたときなの不思議すぎる

--- 110_Vue/040_Reactivity（Track編 Part.1）/start/reactive.js
+++ 110_Vue/040_Reactivity（Track編 Part.1）/start/reactive.js
@@ -1,26 +1,50 @@
 const handler = {
   get(target, key, receiver) {
     const res = Reflect.get(target, key, receiver);
-    console.log('%c[reactive:get]', 'background: green; color: white;', key, res);
+    console.log(
+      "%c[reactive:get]",
+      "background: green; color: white;",
+      key,
+      res
+    );
+    track(target);
     return res;
   },
   set(target, key, value, receiver) {
     const res = Reflect.set(target, key, value, receiver);
-    console.log('%c[reactive:set]', 'background: red; color: white;', key, value);
-    trigger();
+    console.log(
+      "%c[reactive:set]",
+      "background: red; color: white;",
+      key,
+      value
+    );
+    trigger(target);
     return res;
-  }
-}
+  },
+};
 function reactive(target) {
   return new Proxy(target, handler);
+}
+
+const targetMap = new WeakMap();
+function track(target) {
+  console.log(
+    "%c[register:get]",
+    "background: blue; color: white;",
+    target,
+    activeEffect
+  );
+  targetMap.set(target, activeEffect);
 }
 
 let activeEffect = null;
 function effect(fn) {
   activeEffect = fn;
+  activeEffect();
 }
 
-function trigger() {
-  activeEffect();
+function trigger(target) {
+  const effect = targetMap.get(target);
+  effect();
 }
 export { effect, trigger, reactive };
